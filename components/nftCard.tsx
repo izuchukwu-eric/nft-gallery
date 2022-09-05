3c7aaa05fc710cb3ee15ce48c5f84193c90fb282
Izuchukwu Onukwube <57435925+izuchukwu-eric@users.noreply.github.com>
2022-09-05 02:14:46 +0100

    new commit

--- components/nftCard.tsx
+++ components/nftCard.tsx
@@ -16,7 +16,12 @@ const nftCard = ({ nft }: Props) => {
             <h2>{nft.title}</h2>
             <p>{nft.id.tokenId.substr(nft.id.tokenId.length -4)}</p>
             <p>{`${nft.contract.address.substr(0, 5)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
-
+        </div>
+        <div>
+            <p>{nft.description?.substr(0, 150)}</p>
+        </div>
+        <div>
+            <a target={"_blank"} href={`https://etherscan.io/token/${nft.contract.address}`}>View on etherscan</a>
         </div>
     </div>
   )
